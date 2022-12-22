060d53fe6b4296dc8e1eb97522cc8a774bd98778
Fabian Merk <merk@hm.edu>
2022-12-22 22:38:31 +0100

    impl checks for duplicate/removed flights

--- lib/Flights.dart
+++ lib/Flights.dart
@@ -32,46 +32,48 @@ class Flights {
   static void createSingleFlights(jsonContent) {
     Map<String, dynamic> map = jsonContent as Map<String, dynamic>;
     map.forEach((key, value) {
-      String pilotName = value["pilotname"];
-      String copilotName = "";
-      String model = value["planedesignation"];
-      String callsign = value['callsign'];
-      String departureLoc = value["departurelocation"];
-      String arrivalLoc = value["arrivallocation"];
-      String date = value["dateofflight"];
-      int flid = int.parse(value["flid"]);
-      int flightDuration = int.parse(value["flighttime"]);
-      Launchtype launchType = Launchtype.NOT_INIT;
-      launchType = launchType.parseToEnum(value["starttype"]);
-      TimeOfDay departureTime = TimeOfDay(
-          hour: int.parse(value["departuretime"].split(":")[0]),
-          minute: int.parse(value["departuretime"].split(":")[1]));
-      TimeOfDay arrivalTime = TimeOfDay(
-          hour: int.parse(value["arrivaltime"].split(":")[0]),
-          minute: int.parse(value["arrivaltime"].split(":")[1]));
+      if (value["duplicate"] == 0 && value["deleted"] == "0") {
+        String pilotName = value["pilotname"];
+        String copilotName = "";
+        String model = value["planedesignation"];
+        String callsign = value['callsign'];
+        String departureLoc = value["departurelocation"];
+        String arrivalLoc = value["arrivallocation"];
+        String date = value["dateofflight"];
+        int flid = int.parse(value["flid"]);
+        int flightDuration = int.parse(value["flighttime"]);
+        Launchtype launchType = Launchtype.NOT_INIT;
+        launchType = launchType.parseToEnum(value["starttype"]);
+        TimeOfDay departureTime = TimeOfDay(
+            hour: int.parse(value["departuretime"].split(":")[0]),
+            minute: int.parse(value["departuretime"].split(":")[1]));
+        TimeOfDay arrivalTime = TimeOfDay(
+            hour: int.parse(value["arrivaltime"].split(":")[0]),
+            minute: int.parse(value["arrivaltime"].split(":")[1]));
 
-      //Copilot could be pax or FI
-      if (value["finame"] == null) {
-        if (value["attendantname"].toString().isNotEmpty) {
-          copilotName = value["attendantname"];
+        //Copilot could be pax or FI
+        if (value["finame"] == null) {
+          if (value["attendantname"].toString().isNotEmpty) {
+            copilotName = value["attendantname"];
+          }
+        } else {
+          copilotName = value["finame"];
         }
-      } else {
-        copilotName = value["finame"];
+        SingleFlight singleFlight = SingleFlight(
+            pilotName,
+            flid,
+            date,
+            model,
+            callsign,
+            copilotName,
+            launchType,
+            departureLoc,
+            departureTime,
+            arrivalLoc,
+            arrivalTime,
+            flightDuration);
+        listOfFlights[flid] = singleFlight;
       }
-      SingleFlight singleFlight = SingleFlight(
-          pilotName,
-          flid,
-          date,
-          model,
-          callsign,
-          copilotName,
-          launchType,
-          departureLoc,
-          departureTime,
-          arrivalLoc,
-          arrivalTime,
-          flightDuration);
-      listOfFlights[flid] = singleFlight;
     });
     _alreadyBuilt = true;
   }
